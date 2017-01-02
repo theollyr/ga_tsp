3738b724eb7e6359816f2c7136c507b6957165b8
Patrik Gajdošík <gajdosik.patrikk@gmail.com>
2017-01-02 18:10:30 +0100

    allow to use proportional selection (roulette wheel)
    New option for the `parent_selection` variable: proportional.

--- run_ga.m
+++ run_ga.m
@@ -1,8 +1,8 @@
 function run_ga(x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, PR_CROSS, PR_MUT, CROSSOVER, LOCALLOOP, ah1, ah2, ah3)
-% usage: run_ga(x, y, 
-%               NIND, MAXGEN, NVAR, 
-%               ELITIST, STOP_PERCENTAGE, 
-%               PR_CROSS, PR_MUT, CROSSOVER, 
+% usage: run_ga(x, y,
+%               NIND, MAXGEN, NVAR,
+%               ELITIST, STOP_PERCENTAGE,
+%               PR_CROSS, PR_MUT, CROSSOVER,
 %               ah1, ah2, ah3)
 %
 %
@@ -25,7 +25,7 @@ function run_ga(x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, PR_CROSS, PR
         parent_selection = 'ranking';
         fitness_fun = 'tspfun';
         ind_repre = 2;
-        
+
         if ind_repre == 2
             fitness_fun = 'path_fitness';
             CROSSOVER = 'perform_scx';
@@ -34,10 +34,10 @@ function run_ga(x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, PR_CROSS, PR
         GGAP = 1 - ELITIST;
         mean_fits=zeros(1,MAXGEN+1);
         worst=zeros(1,MAXGEN+1);
-        
+
         global Dist
         Dist=zeros(NVAR,NVAR);
-        
+
         for i=1:size(x,1)
             for j=1:size(y,1)
                 Dist(i,j)=sqrt((x(i)-x(j))^2+(y(i)-y(j))^2);
@@ -45,7 +45,7 @@ function run_ga(x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, PR_CROSS, PR
         end
         % initialize population
         Chrom=zeros(NIND,NVAR);
-        
+
         if ind_repre == 1
             for row=1:NIND
                 Chrom(row,:)=path2adj(randperm(NVAR));
@@ -55,7 +55,7 @@ function run_ga(x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, PR_CROSS, PR
                 Chrom(row,:)=randperm(NVAR);
             end
         end
-        
+
         gen=0;
         % number of individuals of equal fitness needed to stop
         stopN=ceil(STOP_PERCENTAGE*NIND);
@@ -74,7 +74,7 @@ function run_ga(x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, PR_CROSS, PR
                     break;
                 end
             end
-            
+
             if ind_repre == 1
                 visualizeTSP(x,y,adj2path(Chrom(t,:)), minimum, ah1, gen, best, mean_fits, worst, ah2, ObjV, NIND, ah3);
             else
@@ -84,18 +84,21 @@ function run_ga(x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, PR_CROSS, PR
             if (sObjV(stopN)-sObjV(1) <= 1e-15)
                   break;
             end
-            
+
             switch parent_selection
                 case 'ranking'
                     % assign fitness values to entire population
                     FitnV=ranking(ObjV);
                     %select individuals for breeding
                     SelCh=select('sus', Chrom, FitnV, GGAP);
-                    
+
                 case 'tournament'
                     SelCh = select('tournament', Chrom, ObjV, GGAP);
+
+                case 'proportional'
+                    SelCh = select('rws', Chrom, ObjV, GGAP);
             end
-            
+
         	%recombine individuals (crossover)
             SelCh = recombin(CROSSOVER,SelCh,PR_CROSS);
             SelCh=mutateTSP('inversion',SelCh,PR_MUT,ind_repre);
@@ -103,9 +106,9 @@ function run_ga(x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, PR_CROSS, PR
         	ObjVSel = feval(fitness_fun, SelCh, Dist);
             %reinsert offspring into population
         	[Chrom ObjV]=reins(Chrom,SelCh,1,1,ObjV,ObjVSel);
-            
+
             Chrom = tsp_ImprovePopulation(NIND, NVAR, Chrom,LOCALLOOP,Dist);
         	%increment generation counter
-        	gen=gen+1;            
+        	gen=gen+1;
         end
 end
