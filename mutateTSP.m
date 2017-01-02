fabd92cfbb3b84d8f2b7f14b81b41e3eeebf227d
Patrik Gajdošík <gajdosik.patrikk@gmail.com>
2017-01-02 02:09:55 +0100

    allow customizable encoding when mutating

--- mutateTSP.m
+++ mutateTSP.m
@@ -1,22 +1,23 @@
 % MUTATETSP.M       (MUTATion for TSP high-level function)
 %
-% This function takes a matrix OldChrom containing the 
+% This function takes a matrix OldChrom containing the
 % representation of the individuals in the current population,
 % mutates the individuals and returns the resulting population.
 %
-% Syntax:  NewChrom = mutate(MUT_F, OldChrom, MutOpt)
+% Syntax:  NewChrom = mutate(MUT_F, OldChrom, MutOpt, Repre)
 %
 % Input parameter:
 %    MUT_F     - String containing the name of the mutation function
 %    OldChrom  - Matrix containing the chromosomes of the old
 %                population. Each line corresponds to one individual.
 %    MutOpt    - mutation rate
+%    Repre     - individual encoding (1 - adj, 2 - path)
 % Output parameter:
 %    NewChrom  - Matrix containing the chromosomes of the population
 %                after mutation in the same format as OldChrom.
 
 
-function NewChrom = mutateTSP(MUT_F, OldChrom, MutOpt);
+function NewChrom = mutateTSP(MUT_F, OldChrom, MutOpt, Repre);
 
 % Check parameter consistency
    if nargin < 2,  error('Not enough input parameters'); end
@@ -26,7 +27,7 @@ NewChrom=OldChrom;
 
 for r=1:rows
 	if rand<MutOpt
-		NewChrom(r,:) = feval(MUT_F, OldChrom(r,:),1);
+		NewChrom(r,:) = feval(MUT_F, OldChrom(r,:),Repre);
 	end
 end
 
